3b4ae083a967193cc42e65ab26c3007c685e4027
Ark21bit <webleif@vk.com>
2024-04-01 12:50:16 +0300

    uno icons add

--- uno.config.ts
+++ uno.config.ts
@@ -1,17 +1,35 @@
-import { defineConfig, presetUno, transformerDirectives, transformerVariantGroup } from 'unocss'
-
+import { defineConfig, presetUno, transformerDirectives, transformerVariantGroup, presetIcons } from 'unocss'
+import { FileSystemIconLoader } from '@iconify/utils/lib/loader/node-loaders'
 export default defineConfig({
-    presets: [
-        presetUno(),
+	presets: [
+		presetUno(),
+		presetIcons({
+			collections: {
+				custom: FileSystemIconLoader(
+					'./assets/iconsUnoCSS',
+					svg => svg.replace(/#50A80A/, 'currentColor')
+				),
+			}
+		})
 	],
-    blocklist: [
-        'container'
-    ],
-    transformers: [
-        transformerDirectives(),
-        transformerVariantGroup()
-    ],
-    rules: [
-        [/^leading-([\.\d]+)$/, ([_, num]) => ({ "line-height": `${num}` })]
-    ]
+	blocklist: [
+		'container'
+	],
+	transformers: [
+		transformerDirectives(),
+		transformerVariantGroup()
+	],
+	theme: {
+		fontFamily: {
+			Onest: 'Onest',
+		},
+		colors: {
+			"tekstovyy": "#2c3e48",
+			"seryy": "#80929c",
+			"firmennyy": "#50a80a",
+		},
+	},
+	rules: [
+		[/^leading-([\.\d]+)$/, ([_, num]) => ({ "line-height": `${num}` })]
+	]
 })
