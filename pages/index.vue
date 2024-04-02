a44f0948c76315764376a86d383182d88229f2d0
Ark21bit <webleif@vk.com>
2024-04-02 21:40:56 +0300

    join-your-restaurant block create

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,48 @@
 	<Catalog title="Каталог блюд" class="mt-11 lg:mt-24" :link="{ to: '/', text: 'Все вкуснейшие блюда' }" :items="catalog" />
 	<Catalog title="Кухня" class="mt-11 lg:mt-24" :link="{ to: '/', text: 'Все кухни мира' }" :items="catalog" />
 	<Restaurant class="mt-11 lg:mt-24" :restaraunts />
+	<div class="grid grid-cols-1 sm:grid-cols-2 gap-8 mt-11 lg:mt-24">
+		<div class="relative flex bg-#F4F8F5 items-center justify-between rounded-5 lg:rounded-10 px-5 lg:px-10 py-5.5 lg:py-8 gap-4 overflow-hidden">
+			<div class="flex flex-col gap-4 lg:gap-5 shrink-0">
+				<h2 class="text-tekstovyy text-6 lg:text-7 font-700 leading-1.1 -tracking-0.02em max-w-73">Присоединяйте свой ресторан</h2>
+				<p class="text-seryy text-4.5 leading-1.2 -tracking-0.02em max-lg:max-w-43 max-w-73">Привлекайте новых посетителей и расширяйте зону продажи</p>
+				<Button class="w-fit">Подключить</Button>
+			</div>
+			<img class="mix-blend-darken h-50 lg:h-47 max-lg:(absolute translate-x-1/3 right-0)" src="/imgs/join-your-restaurant1.png" alt="">
+		</div>
+		<div class="relative flex bg-#F4F8F5 items-center justify-between rounded-5 lg:rounded-10 px-5 lg:px-10 py-5.5 lg:py-8 gap-4 overflow-hidden">
+			<div class="flex flex-col gap-4 lg:gap-5 shrink-0">
+				<h2 class="text-tekstovyy text-6 lg:text-7 font-700 leading-1.1 -tracking-0.02em max-w-73">Займёмся доставкой ваших заказов</h2>
+				<p class="text-seryy text-4.5 leading-1.2 -tracking-0.02em max-lg:max-w-43 max-w-73">Предоставим курьеров и эффективную систему доставки</p>
+				<Button class="w-fit">Оставить заявку</Button>
+			</div>
+			<img class="mix-blend-darken h-43 lg:h-47  max-lg:(absolute translate-x-1/3 right-0)" src="/imgs/join-your-restaurant2.png" alt="">
+		</div>
+
+		<div class="relative flex flex-col sm:flex-row bg-#F4F8F5 lg:items-center justify-between rounded-5 lg:rounded-10 px-5 lg:px-10 py-5.5 lg:py-8 gap-4 col-span-full">
+			<div class="flex flex-col gap-4 lg:gap-5">
+				<h2 class="text-tekstovyy text-6 lg:text-7 font-700 leading-1.1 -tracking-0.02em max-w-92">Проще чем забронировать столик в ресторане</h2>
+				<p class="text-seryy text-4.5 leading-1.2 -tracking-0.02em max-w-120">Еда уже почти у вас дома! Скачайте приложение и сделайте заказ в любимом ресторане прямо сейчас</p>
+				<Button class="w-fit">
+					Установить приложение
+					<template #suffix>
+						<span class="i-custom:full-arrow opacity-60 text-3.5"></span>
+					</template>
+				</Button>
+			</div>
+			<div class="flex max-sm:order-first sm:hidden lg:flex items-end h-full lg:absolute top-0 left-45% border-b border-black/10 lg:border-none w-full lg:w-auto pt-5 lg:pt-0">
+				<img class="lg:h-115% mx-auto" src="/imgs/iPhone_11_Pro_Mockup.png" alt="">
+			</div>
+			<div class="flex items-center gap-2.5 lg:gap-7.5 shrink-0">
+				<div class="flex flex-col gap-2">
+					<NuxtLink to="/"><img width="135" height="40" src="/imgs/app-store.svg" /></NuxtLink>
+					<NuxtLink to="/"><img width="135" height="40" src="/imgs/google-play.svg" /></NuxtLink>
+				</div>
+				<img width="123" height="123" class="size-21.5 lg:size-30.75" src="/imgs/qr-code.svg" />
+			</div>
+		</div>
+	</div>
+
 	<MainEndText class="mt-11 lg:mt-24 max-lg:hidden" />
 </template>
 
