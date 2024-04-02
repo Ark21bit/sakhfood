188bfd6a27cc87e9e042a57d7fb3ba3b5869eeda
Ark21bit <webleif@vk.com>
2024-04-02 21:39:28 +0300

    footer margin fix

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
 	<main class="grid-container grow auto-rows-min">
 		<NuxtPage />
 	</main>
-	<Footer class="mt-22.5" />
+	<Footer class="mt-10 lg:mt-22.5" />
 </template>
 <script lang="ts" setup>
 </script>
