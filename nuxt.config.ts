440a8a969181a4eabb90154f20ec3da3459b7868
Ark21bit <webleif@vk.com>
2024-04-03 16:35:07 +0300

    swiper install

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
 	devtools: { enabled: true },
 	modules: ['@unocss/nuxt', '@nuxt/fonts'],
-	css: ['~/assets/css/main.css'],
+	css: ['swiper/css', '~/assets/css/main.css'],
 	components: [
 		{ path: "~/components/atoms", pathPrefix: false },
 		"~/components",
