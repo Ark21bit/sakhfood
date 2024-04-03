1fe66bf97a0face5e4a72d5f81f277c40d8ca964
Ark21bit <webleif@vk.com>
2024-04-03 19:02:28 +0300

    vueuse install

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-	modules: ['@unocss/nuxt', '@nuxt/fonts'],
+	modules: ['@unocss/nuxt', '@nuxt/fonts', '@vueuse/nuxt'],
 	css: ['swiper/css', '~/assets/css/main.css'],
 	components: [
 		{ path: "~/components/atoms", pathPrefix: false },
