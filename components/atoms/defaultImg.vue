6d8e1edb302f38824514bb1382cecb95b3b40f53
Ark21bit <webleif@vk.com>
2024-04-04 19:45:02 +0300

    defaultImg create

---
+++ components/atoms/defaultImg.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="flex items-center justify-center gradient-default-img">
+		<div class="rounded-2 bg-white p-3 text-#a9aba5 text-4.25 font-500 shadow-default-img -rotate-1">Нет фото</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
--- uno.config.ts
+++ uno.config.ts
@@ -28,8 +28,10 @@ export default defineConfig({
 			"seryy": "#80929c",
 			"firmennyy": "#50a80a",
 		},
-		boxShadow:{
-			header:'0 9px 20px 0 rgba(166, 166, 166, 0.05), 0 37px 37px 0 rgba(166, 166, 166, 0.04), 0 83px 50px 0 rgba(166, 166, 166, 0.03), 0 147px 59px 0 rgba(166, 166, 166, 0.01), 0 230px 64px 0 rgba(166, 166, 166, 0);'
+
+		boxShadow: {
+			header: '0 9px 20px 0 rgba(166, 166, 166, 0.05), 0 37px 37px 0 rgba(166, 166, 166, 0.04), 0 83px 50px 0 rgba(166, 166, 166, 0.03), 0 147px 59px 0 rgba(166, 166, 166, 0.01), 0 230px 64px 0 rgba(166, 166, 166, 0);',
+			"default-img": "0 2px 5px 0 rgba(56, 61, 41, 0.05), 0 10px 10px 0 rgba(56, 61, 41, 0.04), 0 22px 13px 0 rgba(56, 61, 41, 0.03), 0 39px 16px 0 rgba(56, 61, 41, 0.01), 0 61px 17px 0 rgba(56, 61, 41, 0);"
 		}
 	},
 	rules: [
