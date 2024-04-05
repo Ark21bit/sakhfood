e68709e46e4f7703ad09a0d114807958eed6b7cc
Ark21bit <webleif@vk.com>
2024-04-05 17:39:02 +0300

    button add new color && size

--- components/atoms/Button.vue
+++ components/atoms/Button.vue
@@ -11,8 +11,8 @@
 </template>
 
 <script lang="ts" setup>
-type ButtonColor = 'green' | 'seryy' | 'white'
-type ButtonSize = 'sm' | 'md'
+type ButtonColor = 'green' | 'seryy' | 'white' | 'green-light'
+type ButtonSize = 'sm' | 'md' | 'xs'
 type ButtonVariant = 'outline' | 'solid'
 const props = defineProps({
 	tag: { type: [Object, String], default: 'button' },
@@ -41,6 +41,10 @@ const suffixIconName = computed(() => {
 
 const ui = {
 	color: {
+		"green-light": {
+			solid: 'bg-firmennyy/10 text-firmennyy',
+			outline: 'bg-transparent ring-1.5 ring-inset ring-firmennyy/50 text-firmennyy',
+		},
 		green: {
 			solid: 'bg-firmennyy text-white',
 			outline: 'bg-transparent ring-1.5 ring-inset ring-firmennyy/50 text-firmennyy',
@@ -55,21 +59,25 @@ const ui = {
 		}
 	},
 	size: {
+		xs: 'text-3.75 leading-0.8 -tracking-0.01em',
 		sm: 'text-4 lg:text-4 leading-0.8 -tracking-0.01em',
 		md: 'text-4 lg:text-4.5 leading-0.8 -tracking-0.01em',
 	},
 	gap: {
+		xs: 'gap-2',
 		sm: 'gap-1.5',
 		md: 'gap-1.5',
 	},
 	padding: {
+		xs: 'py-2.75 px-7',
 		sm: 'py-3.5 lg:py-3.75 px-7',
 		md: 'py-3.5 lg:py-3.75 px-7',
 	},
-	base: 'flex shrink-0 items-center',
+	base: 'flex shrink-0 items-center justify-center',
 	icon: {
 		base: "shrink-0",
 		size: {
+			xs: 'text-4',
 			sm: "text-4",
 			md: "text-4",
 		}
@@ -85,7 +93,7 @@ const iconClass = computed(() => {
 const buttonClass = computed(() => {
 	const variant = ui.color?.[props.color]?.[props.variant]
 	return [
-		`${ui.base} ${!slots?.suffix && !slots?.prefix ? 'justify-center' : 'justify-between'}`,
+		ui.base,
 		ui.font,
 		ui.rounded,
 		ui.size[props.size],
