1a02cc6d49afd7b35a74d56382bd4c08e7b033a1
Ark21bit <webleif@vk.com>
2024-04-05 17:39:37 +0300

    default image size classes add

--- components/atoms/defaultImg.vue
+++ components/atoms/defaultImg.vue
@@ -1,9 +1,15 @@
 <template>
 	<div class="flex items-center justify-center gradient-default-img">
-		<div class="rounded-2 bg-white p-3 text-#a9aba5 text-4.25 font-500 shadow-default-img -rotate-1">Нет фото</div>
+		<div class="font-500 bg-white text-#a9aba5 -rotate-1" :class="sizeclass?.[size]">Нет фото</div>
 	</div>
 </template>
 
 <script setup lang="ts">
-
+defineProps({
+	size: { type: String as PropType<'sm' | 'md'>, default: 'md' }
+})
+const sizeclass = {
+	sm:'rounded-1 px-1.5 py-1.25 text-2 shadow-default-img-sm',
+	md:'rounded-2 p-3 text-4.25 shadow-default-img-md'
+}
 </script>
