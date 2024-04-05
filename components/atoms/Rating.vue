8a30d8be1a0cc0c76a442913939a20f093fc5ffe
Ark21bit <webleif@vk.com>
2024-04-05 17:39:54 +0300

    create rating component

---
+++ components/atoms/Rating.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="flex gap-0.75">
+		<span v-for="n in rating" class="i-custom:star" :class="[colorClass, size]"></span>
+		<span v-for="n in count - rating" class="i-custom:star-outline" :class="[colorClass, size]"></span>
+	</div>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+	rating: { type: Number, default: 3 },
+	count: { type: Number, default: 5 },
+	size: { type: String, default: "text-4.5" },
+	colorClass: { type: String, default: "text-firmennyy" }
+})
+</script>
